21e068ad9d9e1768741dad1104a9974087740a72
Haui <luigfmp@gmail.com>
2025-09-30 23:12:46 -0500

    chore: :tada: Ignore .env file

---
+++ src/main/java/com/nami/demo/NamiNamiApplication.java
@@ -0,0 +1,14 @@
+package com.nami.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NamiNamiApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(NamiNamiApplication.class, args);
+	}
+
+}
