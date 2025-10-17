7e6803342016c0f343c36226dbe9f75c0a476332
Haui <luigfmp@gmail.com>
2025-10-17 16:43:52 -0500

    agregando la ruta publica health

--- src/main/java/com/nami/demo/config/SecurityMiddleware.java
+++ src/main/java/com/nami/demo/config/SecurityMiddleware.java
@@ -32,7 +32,8 @@ public class SecurityMiddleware {
                 .requestMatchers(
                     "/auth/**",
                     "/docs/**",
-                    "/static/**"
+                    "/static/**",
+                    "/health/**"
                 ).permitAll()
                 .anyRequest().authenticated()
             )
